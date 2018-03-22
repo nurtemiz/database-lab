76c86e9b63b60dce40ca2b855065c94e65a556a9
Nur Temiz <nur.temiz@bil.omu.edu.tr>
2018-03-22 14:51:28 +0300

    update build_schema.sql

--- sql-exercise/exercise_8/build_schema.sql
+++ sql-exercise/exercise_8/build_schema.sql
@@ -15,4 +15,20 @@ CREATE TABLE ogrenci
 --Ogrenci tablosuna veri girilmesi
 INSERT INTO Ogrenci
 VALUES (14060294, 'NUR', 'TEMİZ', '1996-04-29 00:00:00.0000000', 'Samsun', '5070273279', 1, 70, 70),
-	   (15060295, 'ÖZGÜR', 'ÖZDEMİR', '1997-05-29 00:00:00.0000000', 'Samsun', '5050275259', 1, 90, 90);
+       (15060295, 'ÖZGÜR', 'ÖZDEMİR', '1997-05-29 00:00:00.0000000', 'Samsun', '5050275259', 1, 90, 90);
+       
+
+--OgrenciYedek tablosunun oluşturulması
+CREATE TABLE ogrenciYedek
+( 
+	ogrenciID INT PRIMARY KEY NOT NULL,
+	ad NVARCHAR(20),
+	soyad NVARCHAR(20),
+	dTarihi DATE,
+	adres NVARCHAR(20),
+	telefon NVARCHAR(10),
+	bolumID INT,
+	vizeNotu INT,
+	finalNotu INT
+)
+
